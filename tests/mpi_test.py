5bc5c36bebb026944faaa4b656360775fb152148
Vladimir <v.igoshin01@mail.ru>
2022-05-08 21:58:16 +0300

    Update mpi_test.py

--- tests/mpi_test.py
+++ tests/mpi_test.py
@@ -7,20 +7,21 @@ import sys
 
 worker = pckit.SimpleMPIWorker(model=pckit.TestModel())
 
-if len(sys.argv) == 2:
-    test_type = sys.argv[1]
-    if test_type == 'basic_test_cache':
-        basic_test_cache(worker)
-    elif test_type == 'basic_test_solve':
-        basic_test_solve(worker)
-else:
-    with pckit.get_solver(worker=worker) as solver:
-        assert isinstance(solver, pckit.MPISolver)
-        comm = MPI.COMM_WORLD
-        # amount spawned
-        assert comm.Get_size() - 1 == solver.total_workers
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        test_type = sys.argv[1]
+        if test_type == 'basic_test_cache':
+            basic_test_cache(worker)
+        elif test_type == 'basic_test_solve':
+            basic_test_solve(worker)
+    else:
+        with pckit.get_solver(worker=worker) as solver:
+            assert isinstance(solver, pckit.MPISolver)
+            comm = MPI.COMM_WORLD
+            # amount spawned
+            assert comm.Get_size() - 1 == solver.total_workers
 
-        tasks = [pckit.task.Task(1), pckit.task.Task(0)]
-        res = solver.solve(tasks)
-        # right results
-        assert res[0] == 1 and res[1] == 0
+            tasks = [pckit.task.Task(1), pckit.task.Task(0)]
+            res = solver.solve(tasks)
+            # right results
+            assert res[0] == 1 and res[1] == 0
