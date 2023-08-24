2d5dc531ba0f9f4b77109333db0745dd1373c987
djiboshin <64642744+djiboshin@users.noreply.github.com>
2023-08-25 00:27:01 +0300

    Update utils.py

--- pckit/utils.py
+++ pckit/utils.py
@@ -2,6 +2,10 @@
 This module contains different useful classes
 """
 import logging
+import multiprocessing
+import socket
+from ._mpi_check import mpi_function
+
 try:
     from mpi4py import MPI
 except ModuleNotFoundError:
@@ -9,6 +13,8 @@ except ModuleNotFoundError:
 
 
 class MPIFileStream:
+    """File stream. See https://gist.github.com/sixy6e/ed35ea88ba0627e0f7dfdf115a3bf4d1"""
+    @mpi_function
     def __init__(self, filename, amode: int, comm: 'MPI.Intracomm', encoding: str = 'utf-8'):
         self.file = MPI.File.Open(comm=comm, filename=filename, amode=amode)
         self.file.Set_atomicity(flag=True)
@@ -26,6 +32,8 @@ class MPIFileStream:
 
 
 class MPIFileHandler(logging.FileHandler):
+    """FileHandler implementation supports writing in single file"""
+    @mpi_function
     def __init__(
             self,
             filename,
@@ -46,3 +54,32 @@ class MPIFileHandler(logging.FileHandler):
             comm=self.comm,
             encoding=encoding,
         )
+
+
+class MPIRecordFactory:
+    """Class for logging with rank and hostname in format"""
+    @mpi_function
+    def __init__(self, comm: 'MPI.Intracomm' = None) -> None:
+        self.default_factory = logging.getLogRecordFactory()
+        self.rank = MPI.COMM_WORLD.Get_rank() if comm is None else comm
+        self.hostname = socket.gethostname()
+
+    def __call__(self, *args, **kwargs) -> logging.LogRecord:
+        record = self.default_factory(*args, **kwargs)
+        record.rank = self.rank
+        record.hostname = self.hostname
+        return record
+
+
+class MultiprocessingRecordFactory:
+    """Class for logging with process name and hostname in format"""
+    def __init__(self) -> None:
+        self.default_factory = logging.getLogRecordFactory()
+        self.proc_name = multiprocessing.current_process().name
+        self.hostname = socket.gethostname()
+
+    def __call__(self, *args, **kwargs) -> logging.LogRecord:
+        record = self.default_factory(*args, **kwargs)
+        record.proc_name = self.proc_name
+        record.hostname = self.hostname
+        return record
