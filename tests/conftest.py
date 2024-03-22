c499cf91f42aa79ce848704c29ddc44ee7363fee
djiboshin <64642744+djiboshin@users.noreply.github.com>
2024-03-22 04:38:22 +0300

    pytest hangs on linux fix

---
+++ tests/conftest.py
@@ -0,0 +1,8 @@
+import pytest
+
+
+@pytest.fixture(scope='session', autouse=True)
+def set_multiprocessing_start_method():
+    # https://github.com/pytest-dev/pytest/issues/11174
+    import multiprocessing as mp
+    mp.set_start_method("spawn", force=True)
